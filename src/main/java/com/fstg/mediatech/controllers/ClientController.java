5dd68803dc919d3fcc266ad00719970e8e6f7453
AbidarYassine <71972349+AbidarYassine@users.noreply.github.com>
2021-09-06 19:27:29 +0100

    add putMapping deleteMapping

--- src/main/java/com/fstg/mediatech/controllers/ClientController.java
+++ src/main/java/com/fstg/mediatech/controllers/ClientController.java
@@ -1,7 +1,11 @@
 package com.fstg.mediatech.controllers;
+
 import com.fstg.mediatech.dto.ClientRequestDto;
 import com.fstg.mediatech.dto.ClientResponseDto;
 import com.fstg.mediatech.service.ClientService;
+import javassist.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,13 +21,38 @@ public class ClientController {
     }
 
     @GetMapping("")
-    public List<ClientResponseDto> getClient() {
-        return clientService.findAll();
+    public ResponseEntity<List<ClientResponseDto>> getClient() {
+        return new ResponseEntity<>(clientService.findAll(), HttpStatus.OK);
     }
 
     @PostMapping("")
-    public ClientResponseDto save(@RequestBody() ClientRequestDto clientRequestDto) {
-        return clientService.save(clientRequestDto);
+    public ResponseEntity<ClientResponseDto> save(@RequestBody() ClientRequestDto clientRequestDto) {
+        ClientResponseDto clientResponseDto = clientService.save(clientRequestDto);
+        return new ResponseEntity<>(clientResponseDto, HttpStatus.CREATED);
     }
 
+    @GetMapping("/id/{id}")
+    public ResponseEntity<ClientResponseDto> findById(@PathVariable("id") Integer id) {
+        ClientResponseDto clientResponseDto = clientService.findById(id);
+        return ResponseEntity.ok(clientResponseDto);
+
+    }
+
+    @GetMapping("/nom/{nom}")
+    public ResponseEntity<ClientResponseDto> findByNom(@PathVariable() String nom) {
+        ClientResponseDto clientResponseDto = clientService.findByNom(nom);
+        return ResponseEntity.ok(clientResponseDto);
+    }
+
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<?> delete(@PathVariable() Integer id) {
+        clientService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/id/{id}")
+    public ResponseEntity<ClientResponseDto> update(@RequestBody() ClientRequestDto clientRequestDto, @PathVariable() Integer id) throws NotFoundException {
+        ClientResponseDto clientResponseDto = clientService.update(clientRequestDto, id);
+        return ResponseEntity.accepted().body(clientResponseDto);
+    }
 }
